c83d8ff4b8a889b4b3eb14a1e57b076582aa5275
1234Sumuu <41938215+1234Sumuu@users.noreply.github.com>
2023-05-15 12:02:42 +0600

    findMinssingNum.java

--- findMinssingNum.java
+++ findMinssingNum.java
@@ -1,1 +1,18 @@
-
+public class missingNumberFinder {
+  public static int findMissingNumber(int[] nums){
+    int n = nums.length + 1;
+    int totalSum = (n * (n+1)) /2;
+    
+    int arraySum = 0;
+    for(int num : nums){
+      arraySum += n;
+    }
+    return totalSum - arraySum;
+  }
+  public static void main (String[] args){
+    int[] nums = {1, 2, 3, 5, 6};
+    int misingNumber = findMissingNumber(nums);
+    
+    System.out.println("Misiing number is: " + missingNumber);
+  }
+}
